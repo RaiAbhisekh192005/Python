e7c8c0ebc56062d517621b4b33f94cd4be62ba3c
Abhishek Kumar Rai <129398745+RaiAbhisekh192005@users.noreply.github.com>
2024-04-26 07:54:47 +0530

    Add files via upload

---
+++ cripto.py
@@ -0,0 +1,6 @@
+import hashlib
+cripto = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000
+#cripto.update(b'no body inpect')
+#cripto.update(b'i am abhishek')
+#cripto.digest()
+cripto.hex()
